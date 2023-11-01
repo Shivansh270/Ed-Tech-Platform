3718b480e33b3db97dfed38c3ec964baa7388733
Shivansh270 <gupta.shivansh2709@gmail.com>
2023-11-01 21:44:37 +0530

    forgot password ui

---
+++ src/services/operations/authAPI.js
@@ -0,0 +1,30 @@
+import { apiConnector } from "../apiconnector";
+import { endpoints } from "../apis";
+4;
+import { setLoading, setToken } from "../../slices/authSlice";
+import { useDispatch } from "react-redux";
+
+const dispatch = useDispatch();
+export function getPasswordResetToken(email, setEmailSent) {
+  return async (dispatch) => {
+    dispatch(setLoading(true));
+    try {
+      const response = await apiConnector("POST", RESETPASSTOKEN_API, {
+        email,
+      });
+
+      console.log("RESET PASSWORD TOKEN RESPONSE....", response);
+
+      if (!response.data.success) {
+        throw new Error(response.data.message);
+      }
+
+      toast.success("Reset Email Sent");
+      setEmailSent(true);
+    } catch (error) {
+      console.log("RESET PASSWORD TOKEN Error", error);
+      toast.error("Failed to send email for resetting password");
+    }
+    dispatch(setLoading(false));
+  };
+}

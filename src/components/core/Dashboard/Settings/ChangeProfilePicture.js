7bcb1db65bfa2add516a86b821f63eb96d94f965
Shivansh270 <gupta.shivansh2709@gmail.com>
2024-01-22 22:48:34 +0530

    change profile photo code

--- src/components/core/Dashboard/Settings/ChangeProfilePicture.js
+++ src/components/core/Dashboard/Settings/ChangeProfilePicture.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import IconBtn from "../../../common/IconBtn";
 
 const ChangeProfilePicture = () => {
   const user = useSelector((state) => state.profile);
@@ -8,6 +9,18 @@ const ChangeProfilePicture = () => {
   const [loading, setLoading] = useState(false);
   const [imageFile, setImageFile] = useState(null);
   const [previewSource, setPreviewSource] = useState(null);
+
+  const handleClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setPreviewSource(file);
+    }
+  };
 
   const fileInputRef = useRef(null);
   return (
@@ -17,6 +30,31 @@ const ChangeProfilePicture = () => {
           src={previewSource || user?.image}
           alt={`profile-${user?.firstName}`}
         />
+        <div>
+          <p>Change profile picture</p>
+          <div>
+            <input
+              type="file"
+              ref={fileInputRef}
+              onChange={handleFileChange}
+              className="hidden"
+              accept="image/png, image/gif, image/jpeg"
+            />
+            <button
+              onClick={handleClick}
+              disabled={loading}
+              className="cursor-pointer rounded-md bg-richblack-700 py-2 px-5 font-semibold text-richblack-50"
+            >
+              Select
+            </button>
+            <IconBtn
+              text={loading ? "uploading" : "upload"}
+              onclick={handleFileUpload}
+            >
+              {!loading && <FiUpload className="text-lg text-richblack-900" />}
+            </IconBtn>
+          </div>
+        </div>
       </div>
     </div>
   );

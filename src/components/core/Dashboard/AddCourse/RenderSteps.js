0dbd82f4d64ef51454f83ca228c5992f603697bc
Shivansh270 <gupta.shivansh2709@gmail.com>
2024-02-02 22:27:28 +0530

    course creation step2

--- src/components/core/Dashboard/AddCourse/RenderSteps.js
+++ src/components/core/Dashboard/AddCourse/RenderSteps.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { FaCheck } from "react-icons/fa";
 import { useSelector } from "react-redux";
-// import CourseBuilderForm from "./CourseBuilder/CourseBuilderForm";
 import CourseInformationForm from "./CourseInformation/CourseInformationForm";
+import CourseBuilderForm from "./CourseBuilder/CourseBuilderForm";
 
 const RenderSteps = () => {
   const { step } = useSelector((state) => state.course);
@@ -53,7 +53,7 @@ const RenderSteps = () => {
       </div>
 
       {step === 1 && <CourseInformationForm />}
-      {/* {step === 2 && <CourseBuilderForm />} */}
+      {step === 2 && <CourseBuilderForm />}
       {/* {step===3 && <PublishCourse/>} */}
     </>
   );

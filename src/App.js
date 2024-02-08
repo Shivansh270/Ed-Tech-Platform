357ab5732732f58c7cd158ecda3f5018ee7b17ef
Shivansh270 <gupta.shivansh2709@gmail.com>
2024-02-08 23:07:37 +0530

    my course page

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import Cart from "./components/core/Dashboard/Cart";
 import { ACCOUNT_TYPE } from "./utils/constants";
 import { useSelector } from "react-redux";
 import AddCourse from "./components/core/Dashboard/AddCourse";
+import MyCourses from "./components/core/Dashboard/MyCourses";
 
 function App() {
   const { user } = useSelector((state) => state.profile);
@@ -106,6 +107,7 @@ function App() {
           {user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
             <>
               <Route path="dashboard/add-course" element={<AddCourse />} />
+              <Route path="dashboard/my-courses" element={<MyCourses />} />
             </>
           )}
         </Route>

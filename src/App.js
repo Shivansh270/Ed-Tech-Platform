4f462514dc306d050201d4ba5eb7539675e0b285
Shivansh270 <gupta.shivansh2709@gmail.com>
2024-02-10 19:33:39 +0530

    catalog section as per category

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import { useSelector } from "react-redux";
 import AddCourse from "./components/core/Dashboard/AddCourse";
 import MyCourses from "./components/core/Dashboard/MyCourses";
 import EditCourse from "./components/core/Dashboard/EditCourse";
+import Catalog from "./pages/Catalog";
 
 function App() {
   const { user } = useSelector((state) => state.profile);
@@ -31,6 +32,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/catalog/:catalogName" element={<Catalog />} />
         <Route
           path="signup"
           element={

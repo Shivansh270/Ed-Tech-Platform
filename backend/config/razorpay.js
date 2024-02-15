8ed5fd7d96604c8036c7c62e1d0d246e2c554b54
Shivansh270 <gupta.shivansh2709@gmail.com>
2024-02-15 22:36:36 +0530

    styling added

--- backend/config/razorpay.js
+++ backend/config/razorpay.js
@@ -1,8 +1,7 @@
 const Razorpay = require("razorpay");
-require("dotenv").config();
 
-var instance = new Razorpay({
-  key_id: process.env.YOUR_KEY_ID,
-  key_secret: process.env.YOUR_KEY_SECRET,
-  headers: { "X-Razorpay-Account": "acc_Ef7ArAsdU5t0XL" },
+
+exports.instance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY,
+    key_secret: process.env.RAZORPAY_SECRET,
 });
--- src/components/core/HomePage/HighlightText.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HighlightText = ({ text }) => {
-  return <span className="font-bold text-pink-500"> {text}</span>;
-};
-
-export default HighlightText;

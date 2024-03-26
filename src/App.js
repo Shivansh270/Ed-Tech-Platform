a92eb8f9b9d90eee34eef862dab65a5cefcf0095
Shivansh270 <gupta.shivansh2709@gmail.com>
2024-03-26 20:46:38 +0530

    course review slider

--- src/App.js
+++ src/App.js
@@ -81,14 +81,7 @@ function App() {
           }
         />
 
-        <Route
-          path="about"
-          element={
-            <OpenRoute>
-              <About />
-            </OpenRoute>
-          }
-        />
+        <Route path="/about" element={<About />} />
 
         <Route path="/contact" element={<Contact />} />
         <Route
--- src/components/common/ReviewSlider.js
+++ src/components/common/ReviewSlider.js
@@ -4,7 +4,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
-import { Autoplay, FreeMode, Navigation, Pagination } from "swiper";
+// import { Autoplay, FreeMode, Navigation, Pagination } from "swiper";
+import { Navigation, Pagination, Autoplay, FreeMode } from "swiper/modules";
 import ReactStars from "react-rating-stars-component";
 import { apiConnector } from "../../services/apiconnector";
 import { ratingsEndpoints } from "../../services/apis";
@@ -32,8 +33,47 @@ const ReviewSlider = () => {
 
   return (
     <div>
-      <div>
-        <Swiper></Swiper>
+      <div className="h-[190px] text-white">
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={24}
+          loop={true}
+          freeMode={true}
+          autoplay={{
+            delay: 2500,
+          }}
+          modules={[FreeMode, Pagination, Autoplay]}
+          className="w-full"
+        >
+          {reviews.map((review, index) => (
+            <SwiperSlide key={index}>
+              <img
+                src={
+                  review?.user?.image
+                    ? review?.user?.image
+                    : `https://api.dicebear.com/5.x/initials/svg?seed=${review?.user?.firstName} ${review?.user?.lastName}`
+                }
+                alt="Profile Pic"
+                className="h-9 w-9 object-cover rounded-full"
+              />
+              <p>
+                {review?.user?.firstName} {review?.user?.lastName}
+              </p>
+              <p>{review?.course?.courseName}</p>
+              <p>{review?.review}</p>
+              <p>{review?.rating.toFixed(1)}</p>
+              <ReactStars
+                count={5}
+                value={review.rating}
+                size={20}
+                edit={false}
+                activeColor="#ffd700"
+                emptyIcon={<FaStar />}
+                fullIcon={<FaStar />}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );

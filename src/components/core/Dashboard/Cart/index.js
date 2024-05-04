cfb9045a240513114b349989b7d55fdcea1ce305
Shivansh270 <gupta.shivansh2709@gmail.com>
2024-05-04 12:43:04 +0530

    styling and media queries and code addition for dashboard

--- src/components/core/Dashboard/Cart/index.js
+++ src/components/core/Dashboard/Cart/index.js
@@ -1,21 +1,35 @@
-import { useSelector } from "react-redux/es/hooks/useSelector";
+import { useSelector } from "react-redux";
+
 import RenderCartCourses from "./RenderCartCourses";
 import RenderTotalAmount from "./RenderTotalAmount";
 
 export default function Cart() {
-  const { totalItems, total } = useSelector((state) => state.cart);
+  const { total, totalItems } = useSelector((state) => state.cart);
+  const { paymentLoading } = useSelector((state) => state.course);
+
+  if (paymentLoading)
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <div className="spinner"></div>
+      </div>
+    );
+
   return (
-    <div>
-      <h1>Your Cart</h1>
-      <p>{totalItems} courses in cart </p>
+    <>
+      <h1 className="mb-14 text-3xl font-medium text-richblack-5">Cart</h1>
+      <p className="border-b border-b-richblack-400 pb-2 font-semibold text-richblack-400">
+        {totalItems} Courses in Cart
+      </p>
       {total > 0 ? (
-        <div>
+        <div className="mt-8 flex flex-col-reverse items-start gap-x-10 gap-y-6 lg:flex-row">
           <RenderCartCourses />
           <RenderTotalAmount />
         </div>
       ) : (
-        <p>Your Cart is Empty</p>
+        <p className="mt-14 text-center text-3xl text-richblack-100">
+          Your cart is empty
+        </p>
       )}
-    </div>
+    </>
   );
 }
--- src/components/core/Dashboard/InstructorCourses/CoursesTable.js
+++ src/components/core/Dashboard/InstructorCourses/CoursesTable.js
@@ -10,7 +10,7 @@ import { HiClock } from "react-icons/hi";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { useNavigate } from "react-router-dom";
 
-// import { formatDate } from "../../../../services/formatDate";
+import { formatDate } from "../../../../services/formatDate";
 import {
   deleteCourse,
   fetchInstructorCourses,
@@ -92,7 +92,7 @@ export default function CoursesTable({ courses, setCourses }) {
                         : course.courseDescription}
                     </p>
                     <p className="text-[12px] text-white">
-                      {/* Created: {formatDate(course.createdAt)} */}
+                      Created: {formatDate(course.createdAt)}
                     </p>
                     {course.status === COURSE_STATUS.DRAFT ? (
                       <p className="flex w-fit flex-row items-center gap-2 rounded-full bg-richblack-700 px-2 py-[2px] text-[12px] font-medium text-pink-100">
